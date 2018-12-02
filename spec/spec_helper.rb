4bc202384b8b88dabc6b779f0d5db8bdcb2209b1
Alexandre Ruban <alexandre.ruban@gmail.com>
2018-12-02 19:42:51 +0100

    added correct require statements

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 $LOAD_PATH << File.join(File.dirname(__FILE__), "..", "lib")
+$LOAD_PATH << File.join(File.dirname(__FILE__))
 
 require "active_record"
 require "rspec"
 require "my_factory_girl"
+require "models"
